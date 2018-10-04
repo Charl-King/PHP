3e418b6129a442b3ab61fbce24028523024f10c9
Charl King <41908096+Charl-King@users.noreply.github.com>
2018-10-04 20:42:25 +0200

    day 3 almost done, members needs testing

--- Day03/ex03/cookie_crisp.php
+++ Day03/ex03/cookie_crisp.php
@@ -1,4 +1,12 @@
-#!/usr/bin/php
 <?php
-    code_goes_here
+$value = $_GET["value"];
+$name = $GET["name"];
+$action = $GET["action"]
+
+    if ($action == "set")
+        setcookie($name, $value);
+    if ($action == "get" && $_COOKIE[$name])
+        echo $_COOKIE[$name]."\n";
+    if ($action == "del")
+        setcookie($name, "", time() + 86400);
 ?>
